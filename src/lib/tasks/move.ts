5e112cec6ac80ddcb301bf96b755a91336633aa7
izno.tech <iznotek@gmail.com>
2021-05-16 00:40:06 +0200

    move review

--- src/lib/tasks/move.ts
+++ src/lib/tasks/move.ts
@@ -5,7 +5,7 @@ import { asArray } from '../utils';
 
 export function moveTask(from: string | string[], to: string): StringTask<MoveResult> {
    return {
-      commands: ['mv', '-v', ...asArray(from), to],
+      commands: ['mv', '--hard', ...asArray(from), to],
       format: 'utf-8',
       parser: parseMoveResult,
    };
