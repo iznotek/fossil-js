a8221255a1c24e5d84c58afe18e103cf6708a42d
izno.tech <iznotek@gmail.com>
2021-05-28 00:17:39 +0200

    remove test suite for now

--- test/__fixtures__/like.ts
+++
@@ -1,4 +0,0 @@
-
-export function like<T> (what: T) {
-   return expect.objectContaining(what);
-}
--- test/__fixtures__/wait.ts
+++
@@ -1,8 +0,0 @@
-
-export function wait (timeoutOrPromise: number | Promise<unknown> = 10): Promise<void> {
-   if (timeoutOrPromise && typeof timeoutOrPromise === 'object' && typeof timeoutOrPromise.then === 'function') {
-      return timeoutOrPromise.then(() => wait());
-   }
-
-   return new Promise(ok => setTimeout(ok, typeof timeoutOrPromise === 'number' ? timeoutOrPromise : 10));
-}
